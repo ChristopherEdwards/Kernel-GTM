79be4b2bce3d7a3631c3414acaa445e51d27bc64
Sam Habiel <sam.habiel@gmail.com>
2015-07-12 18:33:16 -0400

    width invalid in this context

--- Kernel/Routines/ZISHGUX.m
+++ Kernel/Routines/ZISHGUX.m
@@ -15,7 +15,7 @@ OPEN(X1,X2,X3,X4,X5,X6) ;SR. Open file
  S $ETRAP="D OPNERR^%ZISH"
  S U="^",X2=$$DEFDIR($G(X2)),X4=$$UP^XLFSTR(X4)
  S Y=$S(X4["A":"append",X4["R":"readonly",X4["W":"newversion",1:"readonly")
- S Y=Y_$S(X4["B":":nowrap:stream:width=32767",$G(X5)&(X4["W"):":WIDTH="_+X5,1:"")
+ S Y=Y_$S(X4["B":":nowrap:stream",$G(X5)&(X4["W"):":WIDTH="_+X5,1:"")
  S:$E(Y)=":" Y=$E(Y,2,999) S %IO=X2_X3,%I2="%IO:"_$S($L(Y):"("_Y_")",1:"")_":3"
  O @%I2 S %T=$T
  I '%T S POP=1 Q
