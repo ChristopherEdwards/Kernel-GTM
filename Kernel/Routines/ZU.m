9dafb19221faa6e53a3949414e89d2b8cf35840b
Sam Habiel <sam.habiel@gmail.com>
2016-12-28 16:45:19 -0800

    ZUGTM changes:
     * If Lloyd's ZSY is present, use it.
     * If FOIA ZSY is present, use it.
     * Otherwise, run a default interrupt that saves all variables.

--- Kernel/Routines/ZU.m
+++ Kernel/Routines/ZU.m
@@ -1,4 +1,4 @@
-ZU ;SF/JLI,RWF - For GT.M, TIE ALL TERMINALS TO THIS ROUTINE!! ;11/24/2003  11:34
+ZU ;SF/JLI,RWF - For GT.M, TIE ALL TERMINALS TO THIS ROUTINE!! ;2016-12-28  4:04 PM
  ;;8.0;KERNEL;**275,419**;Jul 10, 1995;Build 5
  ; for GT.M for VMS & Unix, version 4.3
  ;
@@ -66,13 +66,15 @@ HALT I $D(^XUTL("XQ",$J)) D:$G(DUZ)>0 BYE^XUSCLEAN
  HALT
  ;
 JOBEXAM(%ZPOS) ;
- N %reference S %reference=$REFERENCE
+ I $T(NTRUPT^ZSY)]"" D NTRUPT^ZSY Q 1  ; Lloyd's ZSY
+ I $T(JOBEXAM^ZSY)]"" Q $$JOBEXAM^ZSY(%ZPOS)  ; FOIA improved by Sam
+ ;
+ ; This is the default code in case no ZSY is installed.
  S ^XUTL("XUSYS",$J,0)=$H,^XUTL("XUSYS",$J,"INTERRUPT")=$G(%ZPOS)
  S ^XUTL("XUSYS",$J,"ZMODE")=$ZMODE ; SMH - INTERACTIVE or OTHER
  I %ZPOS'["GTM$DMOD" S ^XUTL("XUSYS",$J,"codeline")=$T(@%ZPOS)
  K ^XUTL("XUSYS",$J,"JE")
- I $G(^XUTL("XUSYS","COMMAND"))'="EXAM" ZSHOW "SD":^XUTL("XUSYS",$J,"JE")
- I $G(^XUTL("XUSYS","COMMAND"))="EXAM" ZSHOW "*":^XUTL("XUSYS",$J,"JE")
+ ZSHOW "*":^XUTL("XUSYS",$J,"JE")
  I $G(^XUTL("XUSYS",$J,"CMD"))="HALT" D H2^XUSCLEAN G HALT^ZU
  Q 1
  ;
